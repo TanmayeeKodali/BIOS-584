cc7cd3f6adcf66ef83b65a970922ad6fea4837d1
Tanmayee Kodali <tanmayee.kodali@emory.edu>
2025-10-31 10:46:05 -0400

    submission

--- Quiz3_main.py
+++ Quiz3_main.py
@@ -2,5 +2,15 @@ from self_py_fun.Quiz3Fun import *
 
 # You can use this .py script to perform debugging task.
 sample_arr_1 = np.array([1,2,3,4,5])
-d_1 = compute_D_partial(sample_arr_1)
-# The correct d_1 should be 5.66.
+sample_arr_2 = np.sin(np.arange(0, 2.1*np.pi, np.pi/10))
+
+# Testing incorrect function
+d_1_wrong = compute_D_partial(sample_arr_1)
+print(f"Incorrect D (compute_D_partial): {d_1_wrong:.2f}")  # Should print 1.85
+
+# Testing correct function (D_correct)
+d_val_1 = compute_D_correct(sample_arr_1)
+print(f"d_val_1: {d_val_1:.2f}")  # Should print 5.66
+
+d_val_2 = compute_D_correct(sample_arr_2)
+print(f"d_val_2: {d_val_2:.2f}")
